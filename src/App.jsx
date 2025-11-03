c0236bbaad143459b3530719731e477cdfc82f8d
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-11-03 16:42:26 +0100

    modification du logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/logo_na.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,23 +8,23 @@ function App() {
 
   return (
     <>
-    <h1>Bienvenue sur mon blog</h1>
+      <h1>Bienvenue sur mon blog</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+        <a href="https://www.coup-de-vieux.fr/wp-content/uploads/2010/09/41369276_tarabas012.jpg" target="_blank">
+          <img src={reactLogo} className="logo" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        
+
       </div>
-      
+
     </>
   )
 }
